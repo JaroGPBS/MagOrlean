e566c1b60dc6b80c892472274c5b4bbee9037aec
Moorwen <moorwen1980@gmail.com>
2025-08-25 01:00:10 +0200

    Update autoLogout.js

--- autoLogout.js
+++ autoLogout.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const now = new Date();
-  if (now.getHours() >= 20) {
+  if (hours > 0 || (hours === 0 && minutes >= 1))
     localStorage.clear();
     if (!window.location.pathname.endsWith("index.html")) {
       window.location.href = "index.html";
     }
   }
 });
+
